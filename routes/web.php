e7d210e209965a639af3fceb55d1c519896119ba
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-19 03:06:55 +0400

    feat: register tool routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
+
+use App\Http\Controllers\Tools\Tool1Controller;
+use App\Http\Controllers\Tools\Tool2Controller;
+use App\Http\Controllers\Tools\Tool3Controller;
 use Illuminate\Support\Facades\Route;
+
 Route::get('/', fn () => view('welcome'));
+
+Route::get('/tool-1', Tool1Controller::class)->name('tools.tool1');
+Route::get('/tool-2', Tool2Controller::class)->name('tools.tool2');
+Route::get('/tool-3', Tool3Controller::class)->name('tools.tool3');
