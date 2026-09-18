e09efc2c4d4ddda8004d1236cd3ed331241333a9
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-19 03:06:45 +0400

    feat: add tool scaffold and navigation

---
+++ resources/views/tools/tool3.blade.php
@@ -0,0 +1,3 @@
+<x-layout title="Инструмент 3">
+    <section class="page"><p class="page__eyebrow">Tool 03</p><h1>Инструмент 3</h1><p class="page__description">Каркас третьего инструмента. Здесь будет его собственная логика и интерфейс.</p></section>
+</x-layout>
