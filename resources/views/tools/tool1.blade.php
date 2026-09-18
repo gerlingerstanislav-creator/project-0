eab76c5d746849f003f6a2e870a7f34b75c7452c
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-19 03:06:40 +0400

    feat: add tool scaffold and navigation

---
+++ resources/views/tools/tool1.blade.php
@@ -0,0 +1,3 @@
+<x-layout title="Инструмент 1">
+    <section class="page"><p class="page__eyebrow">Tool 01</p><h1>Инструмент 1</h1><p class="page__description">Каркас первого инструмента. Здесь будет его собственная логика и интерфейс.</p></section>
+</x-layout>
