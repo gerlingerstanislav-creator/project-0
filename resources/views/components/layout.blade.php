30b342f9cb51347afe3a2ff221f6153e61c9ac83
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-19 04:50:34 +0400

    feat: add responsive site favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon-96.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon-96.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
     <title>{{ $title ?? 'Инструменты' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
