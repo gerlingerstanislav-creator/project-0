09f4a8162ccb9873ebbd472e147f5dd3a91a7ed4
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-19 05:18:49 +0400

    test: update homepage access expectation

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,8 +6,8 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function test_homepage_requires_authentication(): void
+    public function test_homepage_is_public(): void
     {
-        $this->get('/')->assertRedirect(route('login'));
+        $this->get('/')->assertRedirect(route('tools.tool1'));
     }
 }
