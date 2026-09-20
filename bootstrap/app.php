b3859b019d7058745f8d33ab404787a9c60daa50
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 02:33:19 +0400

    feat: register Inertia web middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureUserCanEditStartupIdeas;
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
+
         $middleware->alias([
             'can-edit-ideas' => EnsureUserCanEditStartupIdeas::class,
         ]);
