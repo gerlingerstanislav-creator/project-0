5e02f197956c7c998074e1241dcf5c127b3f440a
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 02:36:23 +0400

    refactor: return Inertia page

--- app/Http/Controllers/Tools/Tool2Controller.php
+++ app/Http/Controllers/Tools/Tool2Controller.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Tools;
 
 use App\Http\Controllers\Controller;
-use Illuminate\View\View;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class Tool2Controller extends Controller
 {
-    public function __invoke(): View
+    public function __invoke(): Response
     {
-        return view('tools.tool2');
+        return Inertia::render('Tool2');
     }
 }
