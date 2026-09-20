dc560359643fc375345b23a0256e7e1bdcc158d3
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-20 21:04:29 +0400

    fix: update weather every minute

--- app/Http/Controllers/Tools/WeatherController.php
+++ app/Http/Controllers/Tools/WeatherController.php
@@ -19,6 +19,7 @@ class WeatherController
                     'longitude' => 48.3866,
                     'photo' => 'https://cdn.tripster.ru/photos/42b7f940-ca5a-480b-8708-5c6e7984921d.jpg',
                 'camera' => 'https://cam-world.ru/en/cams/ulyanovsk-sobornaya-ploschad-00000599',
+                    'cameraLive' => false,
                 ],
                 [
                     'id' => 'moscow',
@@ -28,6 +29,7 @@ class WeatherController
                     'longitude' => 37.6173,
                     'photo' => 'https://hostel24.org/upload/ammina.optimizer/jpg-webp/q80/upload/medialibrary/644/8vgq0vjsp290qhozpy1l504ugqmmnmti.webp',
                 'camera' => 'https://www.geocam.ru/online/taganskaya-square-webcam/',
+                    'cameraLive' => true,
                 ],
                 [
                     'id' => 'munich',
@@ -37,6 +39,7 @@ class WeatherController
                     'longitude' => 11.5820,
                     'photo' => 'https://www.worldplacesexplained.com/r2/og/places/munich-preview.webp',
                 'camera' => 'https://www.munich.travel/en/webcam',
+                    'cameraLive' => true,
                 ],
             ],
         ]);
