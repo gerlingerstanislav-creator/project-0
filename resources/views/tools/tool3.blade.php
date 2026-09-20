55f0cb3e3f0fbcd7071852e1854221b4ed079722
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 02:37:21 +0400

    refactor: remove legacy Blade and page JavaScript

--- resources/views/tools/tool3.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout title="Инструмент 3">
-    <section class="page">
-        <p class="page__eyebrow">Page 03</p>
-        <h1>Инструмент 3</h1>
-        <p class="page__description">Каркас третьего инструмента. Здесь будет его собственная логика и интерфейс.</p>
-    </section>
-</x-layout>
