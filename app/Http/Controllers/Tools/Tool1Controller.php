cd6c9dd045e79f86ece94805d97b8f6fb0314598
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 02:33:11 +0400

    refactor: render startup ideas with Inertia

--- app/Http/Controllers/Tools/Tool1Controller.php
+++ app/Http/Controllers/Tools/Tool1Controller.php
@@ -4,19 +4,24 @@ namespace App\Http\Controllers\Tools;
 
 use App\Http\Controllers\Controller;
 use App\Models\StartupIdea;
+use App\Http\Middleware\EnsureUserCanEditStartupIdeas;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class Tool1Controller extends Controller
 {
-    public function __invoke(): View
+    public function __invoke(): Response
     {
         $ideas = StartupIdea::query()
             ->orderBy('id')
             ->get();
 
-        return view('tools.tool1', compact('ideas'));
+        return Inertia::render('Tool1', [
+            'ideas' => $ideas,
+            'canEditIdeas' => auth()->user()?->canEditIdeas() ?? false,
+        ]);
     }
 
     public function update(Request $request, StartupIdea $startupIdea): JsonResponse
