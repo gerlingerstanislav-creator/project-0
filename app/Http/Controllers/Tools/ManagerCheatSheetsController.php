3a295f8c2383eef4dbcd70293afc2009f8307ff7
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 02:36:28 +0400

    refactor: return Inertia page

--- app/Http/Controllers/Tools/ManagerCheatSheetsController.php
+++ app/Http/Controllers/Tools/ManagerCheatSheetsController.php
@@ -1,6 +1,16 @@
 <?php
+
 namespace App\Http\Controllers\Tools;
+
 use App\Http\Controllers\Controller;
 use App\Models\ManagerCheatSheet;
-use Illuminate\View\View;
-class ManagerCheatSheetsController extends Controller { public function __invoke(): View { $cheatSheets=ManagerCheatSheet::query()->orderBy('id')->get(); return view('tools.manager-cheat-sheets',compact('cheatSheets')); } }
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ManagerCheatSheetsController extends Controller
+{
+    public function __invoke(): Response
+    {
+        return Inertia::render('ManagerCheatSheets', ['cheatSheets' => ManagerCheatSheet::query()->orderBy('id')->get()]);
+    }
+}
