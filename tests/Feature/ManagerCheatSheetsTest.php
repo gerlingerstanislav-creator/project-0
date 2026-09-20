82858917d22b9e6e5561e940586986581f4d0fee
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 01:29:14 +0400

    Fix ski resorts feature test

--- tests/Feature/ManagerCheatSheetsTest.php
+++ tests/Feature/ManagerCheatSheetsTest.php
@@ -25,14 +25,16 @@ class ManagerCheatSheetsTest extends TestCase
             ->assertSee('Проверить факт.');
     }
 
-    public function test_weather_page_is_public_and_contains_all_cities(): void
+    public function test_weather_page_is_public_and_contains_all_ski_resorts(): void
     {
         $this->get('/weather')
             ->assertOk()
-            ->assertSee('Погода')
-            ->assertSee('Ульяновск')
-            ->assertSee('Москва')
-            ->assertSee('Мюнхен')
-            ->assertSee('Обновление автоматически');
+            ->assertSee('Горнолыжные курорты')
+            ->assertSee('Шерегеш')
+            ->assertSee('Красная Поляна')
+            ->assertSee('Роза Хутор')
+            ->assertSee('Газпром')
+            ->assertSee('Большой Вудъявр')
+            ->assertSee('Погода обновляется автоматически');
     }
 }
