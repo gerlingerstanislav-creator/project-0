ef7052573d861c63b41e6af46ca8fd69a41030fc
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 02:36:13 +0400

    refactor: migrate tool 3 to Vue

---
+++ resources/js/pages/Tool3.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '../layouts/AppLayout.vue';
+</script>
+
+<template>
+    <Head title="Инструмент 3" />
+    <AppLayout>
+        <section class="page">
+            <p class="page__eyebrow">Page 03</p>
+            <h1>Инструмент 3</h1>
+            <p class="page__description">Каркас третьего инструмента. Здесь будет его собственная логика и интерфейс.</p>
+        </section>
+    </AppLayout>
+</template>
