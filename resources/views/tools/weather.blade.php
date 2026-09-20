a2da0031521eaade57cab281f457599fa45a37c2
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 01:19:16 +0400

    crop Moscow camera embed to video area

--- resources/views/tools/weather.blade.php
+++ resources/views/tools/weather.blade.php
@@ -18,8 +18,8 @@
                             <a class="weather-card__camera-label" href="{{ $city['camera'] }}" target="_blank" rel="noopener">Источник камеры ↗</a>
                         </div>
                     @elseif ($city['cameraLive'])
-                        <div class="weather-card__photo-link weather-card__photo-link--live">
-                            <iframe class="weather-card__camera-frame" src="{{ $city['cameraEmbed'] ?? $city['camera'] }}" title="Актуальная веб-камера: {{ $city['name'] }}" loading="eager" allow="autoplay; fullscreen; picture-in-picture" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                        <div class="weather-card__photo-link weather-card__photo-link--live{{ $city['id'] === 'moscow' ? ' weather-card__photo-link--moscow' : '' }}">
+                            <iframe class="weather-card__camera-frame{{ $city['id'] === 'moscow' ? ' weather-card__camera-frame--moscow' : '' }}" src="{{ $city['cameraEmbed'] ?? $city['camera'] }}" title="Актуальная веб-камера: {{ $city['name'] }}" loading="eager" allow="autoplay; fullscreen; picture-in-picture" referrerpolicy="strict-origin-when-cross-origin"></iframe>
                             <a class="weather-card__camera-label" href="{{ $city['camera'] }}" target="_blank" rel="noopener">Открыть источник камеры ↗</a>
                         </div>
                     @else
