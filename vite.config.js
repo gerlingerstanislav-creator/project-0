bff9b745bff064335423e2bce8d37f616fd0cb97
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 02:26:29 +0400

    refactor: load JavaScript by page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-export default defineConfig({plugins:[laravel({input:['resources/css/app.css','resources/js/app.js'],refresh:true}),tailwindcss()]});
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/startup-ideas.js',
+                'resources/js/beer-game.js',
+                'resources/js/ski-resort.js',
+            ],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
