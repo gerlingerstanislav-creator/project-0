03c00bf14fdd16d7db77bfccaafe0e62ebc1e752
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 04:27:25 +0400

    refactor: migrate tool pages to design system

--- resources/js/pages/Tool3.vue
+++ resources/js/pages/Tool3.vue
@@ -1,15 +1,13 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import AppLayout from '../layouts/AppLayout.vue';
+import PageHeader from '../components/ui/PageHeader.vue';
 </script>
-
 <template>
     <Head title="Инструмент 3" />
     <AppLayout>
         <section class="page">
-            <p class="page__eyebrow">Page 03</p>
-            <h1>Инструмент 3</h1>
-            <p class="page__description">Каркас третьего инструмента. Здесь будет его собственная логика и интерфейс.</p>
+            <PageHeader eyebrow="03 / TOOL" title="Инструмент 3" description="Каркас третьего инструмента. Здесь будет его собственная логика и интерфейс." />
         </section>
     </AppLayout>
 </template>
