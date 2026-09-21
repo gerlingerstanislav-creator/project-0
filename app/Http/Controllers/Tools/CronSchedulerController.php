762defd0f0503d9338c18602d4bffe6cce609e94
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 15:23:35 +0400

    fix: repair cron scheduler deployment
    Fix malformed cron scheduler PHP files that break Composer autoloading and Laravel route parsing during CI.

--- app/Http/Controllers/Tools/CronSchedulerController.php
+++ app/Http/Controllers/Tools/CronSchedulerController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\\Http\\Controllers\\Tools;
+namespace App\Http\Controllers\Tools;
 
-use App\\Http\\Controllers\\Controller;
-use Inertia\\Inertia;
-use Inertia\\Response;
+use App\Http\Controllers\Controller;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class CronSchedulerController extends Controller
 {
