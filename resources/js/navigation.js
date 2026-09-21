86907c91c2c2737d9ba45260547dad01930eb2cb
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-21 04:10:36 +0400

    refactor: split app navigation and push state

---
+++ resources/js/navigation.js
@@ -0,0 +1,8 @@
+export const appLinks = [
+    { href: '/tool-1', label: 'Идеи стартапов', number: '1' },
+    { href: '/tool-2', label: 'Инструмент 2', number: '2' },
+    { href: '/tool-3', label: 'Инструмент 3', number: '3' },
+    { href: '/manager-cheat-sheets', label: 'Менеджерские шпаргалки', number: '4' },
+    { href: '/ski-resort', label: 'Горнолыжные курорты', number: '5' },
+    { href: '/tests', label: 'Тесты', number: '6' },
+];
